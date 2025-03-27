43e79e9a004028e017a8f58e08aa1bbba90a902e
joaootaviofm <joaootaviofm@gmail.com>
2025-03-27 13:29:34 -0300

    feat: added createHabit in service

--- src/main/java/br/com/habittracker/habittracker/service/HabitService.java
+++ src/main/java/br/com/habittracker/habittracker/service/HabitService.java
@@ -1,16 +1,27 @@
 package br.com.habittracker.habittracker.service;
 
 
+import br.com.habittracker.habittracker.controller.HabitDTO;
+import br.com.habittracker.habittracker.model.Habit;
 import br.com.habittracker.habittracker.repository.HabitRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HabitService {
 
-    HabitRepository repository;
-    HabitService(HabitRepository repository){
-        this.repository = repository;
+    HabitRepository habitRepository;
+    HabitService(HabitRepository habitRepository){
+        this.habitRepository = habitRepository;
     }
 
-    
+    public Long createHabit(HabitDTO habitDTO){
+
+        var entity = new Habit(null,
+                habitDTO.name(),
+                habitDTO.description(),
+                habitDTO.isComplete());
+        var userSaved = habitRepository.save(entity);
+        return userSaved.getId();
+    }
+
 }
